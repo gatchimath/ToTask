acba8658a0c2921e15e647e9d8e9563fd9c39b66
gatchi <christen.got@gmail.com>
2016-11-07 16:28:19 -0800

    added constructors

--- app/src/main/java/com/gatchimath/totask/Task.java
+++ app/src/main/java/com/gatchimath/totask/Task.java
@@ -1,11 +1,39 @@
 package com.gatchimath.totask;
 
+/**
+ * Basic task class.
+ *
+ * @author  gatchi (github.com/gatchi)
+ */
 class Task {
 	
 	private String name;
 	private String text;
 	private int id;
-	private boolean checkState;
+	private boolean checkedState;
+	
+	Task(String name) {
+		this.name = name;
+		this.text = "";
+		//@todo: Generate a unique ID here instead of setting to 0.
+		this.id = 0;
+		this.checkedState = false;
+	}
+	
+	Task(String name, String text) {
+		this.name = name;
+		this.text = text;
+		//@todo  Generate a unique ID here instead of setting to 0.
+		this.id = 0;
+		this.checkedState = false;
+	}
+	
+	Task(String name, String text, int id) {
+		this.name = name;
+		this.text = text;
+		this.id = id;
+		this.checkedState = false;
+	}
 	
 	/**
 	 * @return The name and main content of the task.
