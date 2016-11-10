f80ec9a5651791e78aa88e68bd0b94c68d6855d0
gatchi <christen.got@gmail.com>
2016-11-09 20:34:46 -0800

    removed "id" and extra constructors
    also made public

--- app/src/main/java/com/gatchimath/totask/Task.java
+++ app/src/main/java/com/gatchimath/totask/Task.java
@@ -1,108 +1,79 @@
 package com.gatchimath.totask;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 /**
  * Basic task class.
  *
  * @author  gatchi (github.com/gatchi)
  */
-class Task {
-	
+public class Task {
+
 	private String name;
 	private String text;
-	private int id;
 	private boolean doneState;
 	
-	Task() {
-		this("");
-	}
-	
-	Task(String name) {
-		this(name, "");
-	}
-	
-	Task(String name, String text) {
+	/**
+	 * @param name  The name and main content of this task.
+	 * @param text  Any additional details pertinent to the task.
+	 */
+	public Task(String name, String text) {
 		this.name = name;
 		this.text = text;
-		this.id = 0;
-		this.doneState = false;
-	}
-	
-	Task(String name, String text, int id) {
-		this.name = name;
-		this.text = text;
-		this.id = id;
 		this.doneState = false;
 	}
 	
 	/**
 	 * @return The name and main content of the task.
 	 */
-	String getName() {
+	public String getName() {
 		return name;
 	}
 	
 	/**
 	 * @param name  The name and main content of this task.
 	 */
-	void setName(String name) {
+	public void setName(String name) {
 		this.name = name;
 	}
 	
 	/**
 	 * @return Any additional details pertinent to the task.
 	 */
-	String getText() {
+	public String getText() {
 		return text;
 	}
 	
 	/**
 	 * @param text  Any additional details pertinent to the task.
 	 */
-	void setText(String text) {
+	public void setText(String text) {
 		this.text = text;
 	}
 	
 	/**
 	 * @return True if the task is done, false if not.
 	 */
-	boolean isDone() {
+	public boolean isDone() {
 		return doneState;
 	}
 	
 	/**
 	 * Mark the task as done.
 	 */
-	void setDone() {
-		doneState = true;
+	public void setDone() {
+		setDoneState(true);
 	}
 	
 	/**
 	 * @param value  True if the task has been finished, false if otherwise
 	 */
-	void setDoneState(boolean value) {
+	public void setDoneState(boolean value) {
 		doneState = value;
 	}
 	
 	/**
 	 * Toggles the done state of the task.
 	 */
-	void toggleDoneState() {
+	public void toggleDoneState() {
 		doneState = !doneState;
-	}
-	
-	/**
-	 * @return  The unique ID of the task.
-	 */
-	int getId() {
-		return id;
-	}
-	
-	/**
-	 * @param  The unique ID of the task.
-	 */
-	void setId(int id) {
-		this.id = id;
 	}
 }
