aa036114169580fe6792ad736f5ffe87e7cbb711
gatchi <christen.got@gmail.com>
2016-11-09 20:47:22 -0800

    Task class (#5)
    * added Task interface
    
    * added newline to Task.java cause whine whine whine
    
    * re-organized Task interface
    
    and removed "toggleChecked()" cause it was unnecessary
    
    * reformatted Task interface
    
    now featuring javadoc!
    
    * re-organized again
    
    setText with getText, etc
    
    * converted Task interface into a class
    
    * added getId and setId methods
    
    * add class variables
    
    * added constructors
    
    * renaming: "checked" -> "done", added methods
    
    added and implemented the following:
    + setDone()
    + toggleDoneState()
    
    * implemented the rest of the methods
    
    * code clean
    
    There was still some leftover code from the ID generator.  ID fields are
    still left but default to zero.  May remove completely later.
    
    * removed "id" and extra constructors
    
    also made public

---
+++ app/src/main/java/com/gatchimath/totask/Task.java
@@ -0,0 +1,79 @@
+package com.gatchimath.totask;
+
+/**
+ * Basic task class.
+ *
+ * @author  gatchi (github.com/gatchi)
+ */
+public class Task {
+
+	private String name;
+	private String text;
+	private boolean doneState;
+	
+	/**
+	 * @param name  The name and main content of this task.
+	 * @param text  Any additional details pertinent to the task.
+	 */
+	public Task(String name, String text) {
+		this.name = name;
+		this.text = text;
+		this.doneState = false;
+	}
+	
+	/**
+	 * @return The name and main content of the task.
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * @param name  The name and main content of this task.
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return Any additional details pertinent to the task.
+	 */
+	public String getText() {
+		return text;
+	}
+	
+	/**
+	 * @param text  Any additional details pertinent to the task.
+	 */
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	/**
+	 * @return True if the task is done, false if not.
+	 */
+	public boolean isDone() {
+		return doneState;
+	}
+	
+	/**
+	 * Mark the task as done.
+	 */
+	public void setDone() {
+		setDoneState(true);
+	}
+	
+	/**
+	 * @param value  True if the task has been finished, false if otherwise
+	 */
+	public void setDoneState(boolean value) {
+		doneState = value;
+	}
+	
+	/**
+	 * Toggles the done state of the task.
+	 */
+	public void toggleDoneState() {
+		doneState = !doneState;
+	}
+}
