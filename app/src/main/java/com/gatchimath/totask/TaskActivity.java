b2be4398a4b1c4e64f366443e6f6d5d1d0bda516
Daniel Thornburgh <mysterymath@gmail.com>
2016-11-11 00:42:34 -0800

    Added a trivial RecyclerView

--- app/src/main/java/com/gatchimath/totask/TaskActivity.java
+++ app/src/main/java/com/gatchimath/totask/TaskActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -26,6 +28,13 @@ public class TaskActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        RecyclerView taskList = (RecyclerView)findViewById(R.id.task_list);
+        RecyclerView.LayoutManager taskListLayoutManager = new LinearLayoutManager(this);
+        taskList.setLayoutManager(taskListLayoutManager);
+
+        RecyclerView.Adapter adapter = new MyAdapter(new String[]{"a", "b", "c"});
+        taskList.setAdapter(adapter);
     }
 
     @Override
