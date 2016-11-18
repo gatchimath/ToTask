28f741d16bb65c21f703a7687852c6eff9dea361
gatchi <christen.got@gmail.com>
2016-11-18 13:47:13 -0800

    Updated db definitions
    o Gave useful column names
    + Added "doneness" column
    o Renamed TaskEntry and made own class
    - Removed TaskSqlContract cause now empty

---
+++ app/src/main/java/com/gatchimath/totask/TaskTableEntry.java
@@ -0,0 +1,10 @@
+package com.gatchimath.totask;
+
+import android.provider.BaseColumns;
+
+public class TaskTableEntry implements BaseColumns {
+	public static final String TABLE_NAME = "tasks";
+	public static final String COLUMN_NAME_1 = "name";
+	public static final String COLUMN_NAME_2 = "details";
+	public static final String COLUMN_NAME_3 = "doneness";
+}
