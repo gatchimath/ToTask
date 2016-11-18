09832363df45e90599f67445e19f268a4d44fe03
gatchi <christen.got@gmail.com>
2016-11-18 13:47:13 -0800

    Updated db definitions
    o Gave useful column names
    + Added "doneness" column
    o Renamed TaskEntry and made own class
    - Removed TaskSqlContract cause now empty

--- app/src/main/java/com/gatchimath/totask/TaskDbHelper.java
+++ app/src/main/java/com/gatchimath/totask/TaskDbHelper.java
@@ -5,16 +5,19 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class TaskDbHelper extends SQLiteOpenHelper {
+	
 	private static final String TEXT_TYPE = " TEXT";
+	private static final String BOOL_TYPE = " INTEGER";
 	private static final String COMMA_SEP = ",";
 	
 	private static final String SQL_CREATE_ENTRIES =
-		"CREATE TABLE " + TaskSqlContract.TaskEntry.TABLE_NAME + " (" +
-		TaskSqlContract.TaskEntry._ID + " INTEGER PRIMARY KEY," +
-		TaskSqlContract.TaskEntry.COLUMN_NAME_TITLE + TEXT_TYPE + COMMA_SEP +
-		TaskSqlContract.TaskEntry.COLUMN_NAME_SUBTITLE + TEXT_TYPE + " )";
+		"CREATE TABLE " + TaskTableEntry.TABLE_NAME + " (" +
+		TaskTableEntry._ID + " INTEGER PRIMARY KEY," +
+		TaskTableEntry.COLUMN_NAME_1 + TEXT_TYPE + COMMA_SEP +
+		TaskTableEntry.COLUMN_NAME_2 + TEXT_TYPE + COMMA_SEP +
+		TaskTableEntry.COLUMN_NAME_3 + BOOL_TYPE + " )";
 		
-	public static final int DATABASE_VERSION = 1;
+	public static final int DATABASE_VERSION = 2;
 	public static final String DATABASE_NAME = "ToTask.db";
 	
 	public TaskDbHelper(Context context) {
